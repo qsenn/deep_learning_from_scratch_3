3e4c80ed0fd6e5e51ddb90ed2dc2973044992cb6
qsenn <qsenn1020@gmail.com>
2026-05-01 12:18:37 +0000

    Add from step08 checking variable's data type is ndarray. Add function as_array to convert function's output to ndarray.

---
+++ steps/step09.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} is not supported.')
+        self.data = data
+        self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self) # Set creator for output variable to use when backpropagate.
+        self.input = input # Store input for backpropagation.
+        self.output = output
+        return output
+    
+    def forward(self, x):
+        raise NotImplementedError()
+    
+    def backward(self, gy):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy): # return is just raw data
+        x = self.input.data
+        gx = gy * 2 * x
+        return gx
+
+        
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = gy * np.exp(x)
+        return gx
+
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+x = Variable(np.array(0.5))
+y = square(exp(square(x)))
+y.backward()
+print(x.grad)
+
+x = Variable(np.array(1.0))
+x = Variable(None)
+
+x = Variable(1.0)
