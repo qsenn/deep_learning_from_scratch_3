8f407f1c3736109f6cff2fa38bfd8b137660edd0
qsenn <qsenn1020@gmail.com>
2026-05-19 10:47:50 +0000

    Add step12.py Modified how function classes accept variables of various lengths.

---
+++ steps/step12.py
@@ -0,0 +1,119 @@
+import numpy as np
+import unittest
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f'{type(data)} is not supported.')
+        self.data = data
+        self.grad = None
+        self.creator = None
+    
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+
+class Function:
+    def __call__(self, *inputs): # Use variable argument to allow the functions to accept inputs of various lengths.
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs) # Use variable unpacking to allow forward methods to directly exploit variables, instead of extracting individual variables from a tuple or a list.
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+        for output in outputs:
+            output.set_creator(self) # Set creator for output variable to use during backpropagation.
+        self.inputs = inputs # Store input for backpropagation.
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+    
+    def forward(self, x):
+        raise NotImplementedError()
+    
+    def backward(self, gy):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy): # return is just raw data
+        x = self.input.data
+        gx = gy * 2 * x
+        return gx
+
+        
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = gy * np.exp(x)
+        return gx
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+    
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+    
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1))
+        y = square(x)
+        y.backward()
+        num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+x0 = Variable(np.array(2))
+x1 = Variable(np.array(3))
+y = add(x0, x1)
+print(y.data)
