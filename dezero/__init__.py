8b259961fb057efdb3ddf739c18c3076f891d33b
qsenn <qsenn1020@gmail.com>
2026-06-14 12:47:10 +0000

    Update step22.py, Add step23.py, Add core_simple.py, Add __init__.py  - Fix step22's division part  - Moved most of the code to core_simple.py  - Importing examples in step23.py

---
+++ dezero/__init__.py
@@ -0,0 +1,13 @@
+is_simple_core = True # From step 23 to step 32, this option should be set as True, otherwise set as False.
+
+if is_simple_core:
+    from dezero.core_simple import Variable
+    from dezero.core_simple import Function
+    from dezero.core_simple import using_config
+    from dezero.core_simple import no_grad
+    from dezero.core_simple import as_array
+    from dezero.core_simple import as_variable
+    from dezero.core_simple import setup_variable
+
+
+setup_variable()
--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -1,20 +1,10 @@
 import numpy as np
-import unittest
-import weakref
 import contextlib
+import weakref
+
 
 class Config:
     enable_backprop = True
-
-
-@contextlib.contextmanager
-def using_config(name, value):
-    old_value = getattr(Config, name)
-    setattr(Config, name, value)
-    try:
-        yield
-    finally:
-        setattr(Config, name, old_value)
 
 
 class Variable:
@@ -122,26 +112,6 @@ class Function:
         raise NotImplementedError()
     
 
-class Square(Function):
-    def forward(self, x):
-        return x ** 2
-
-    def backward(self, gy): # return is just raw data
-        x = self.inputs[0].data
-        gx = gy * 2 * x
-        return gx
-
-        
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
-
-    def backward(self, gy):
-        x = self.input.data
-        gx = gy * np.exp(x)
-        return gx
-
-
 class Add(Function):
     def forward(self, x0, x1):
         y = x0 + x1
@@ -202,14 +172,31 @@ class Pow(Function):
         c = self.c
         gx = gy * c * x ** (c - 1)
         return gx
-        
-
-def square(x):
-    return Square()(x)
 
 
-def exp(x):
-    return Exp()(x)
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+def as_variable(obj):
+	if isinstance(obj, Variable):
+		return obj
+	return Variable(obj)
 
 
 def add(x0, x1):
@@ -250,78 +237,14 @@ def pow(x, c):
     return Pow(c)(x)
 
 
-Variable.__mul__ = mul
-Variable.__rmul__ = mul
-Variable.__add__ = add
-Variable.__radd__ = add
-Variable.__neg__ = neg
-Variable.__sub__ = sub
-Variable.__rsub__ = rsub
-Variable.__truediv__ = div
-Variable.__rtruediv__ = rdiv
-Variable.__pow__ = pow
-
-
-def as_array(x):
-    if np.isscalar(x):
-        return np.array(x)
-    return x
-
-def as_variable(obj):
-	if isinstance(obj, Variable):
-		return obj
-	return Variable(obj)
-
-
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
-
-
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
-    
-    def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = square(x)
-        y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
-    
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1))
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
-
-
-def no_grad():
-    return using_config('enable_backprop', False)
-
-x = Variable(np.array(2.0))
-y = -x
-print(y)
-y.backward()
-print(x.grad)
-x.cleargrad()
-y1 = 2.0 - x
-y2 = x - 2.0
-print(y1)
-print(y2)
-y1.backward()
-print(x.grad)
-
-x = Variable(np.array(2.0))
-y = x ** 3
-print(y)
-y.backward()
-print(x.grad)
+def setup_variable():
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__neg__ = neg
+    Variable.__sub__ = sub
+    Variable.__rsub__ = rsub
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__pow__ = pow
